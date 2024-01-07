005b14724c094635430a448374ba7622145fb36f
pauluswi <35646814+pauluswi@users.noreply.github.com>
2024-01-07 18:18:02 +0700

    Update trollfeeding.sol

--- stage-2/trollfeeding.sol
+++ stage-2/trollfeeding.sol
@@ -1,1 +1,39 @@
+pragma solidity ^0.4.25;
+import "./trollfactory.sol";
+contract KatzeInterface {
+  function getKatze(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+contract TrollFeeding is TrollFactory {
 
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KatzeInterface kittyContract = KatzeInterface(ckAddress);
+
+  function feedAndMultiply(uint _trollId, uint _targetDna, string _species) public {
+    require(msg.sender == trollToOwner[_trollId]);
+    Troll storage myTroll = trolls[_trollId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myTroll.dna + _targetDna) / 2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("katze"))) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createTroll("NoName", newDna);
+  }
+
+  function feedOnKatze(uint _trollId, uint _katzeId) public {
+    uint katzeDna;
+    (,,,,,,,,,katzeDna) = katzeContract.getKatze(_katzeId);
+    feedAndMultiply(_trollId, katzeDna, "katze");
+  }
+
+}
